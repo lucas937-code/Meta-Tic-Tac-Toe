14b07ecd8fb7659b2cb57cdd83af93f579db4872
Lucas Hein <lucas.hein.dev@gmail.com>
2025-04-06 13:37:59 +0200

    removed blank lines

--- main.cpp
+++ main.cpp
@@ -9,12 +9,9 @@ int main() {
         std::cerr << "Error occurred while starting the game" << std::endl;
         return 1;
     }
-
     while (!WindowShouldClose()) {
         std::unique_ptr<Game> game = std::make_unique<Game>();
-
         WindowManager::ShowEndScreen(game.operator*(), game->Run());
     }
-
     return 0;
 }
